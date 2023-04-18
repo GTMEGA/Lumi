b8ede0de0d8993a91cd460dd793b9538d7874317
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2023-04-18 21:35:59 +0200

    Initial Commit

---
+++ src/main/java/com/falsepattern/lumina/internal/LUMINA.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 FalsePattern
+ * All Rights Reserved
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.falsepattern.lumina.internal;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Tags.MODID,
+     version = Tags.VERSION,
+     name = Tags.MODNAME,
+     acceptedMinecraftVersions = "[1.7.10]",
+     dependencies = "required-after:falsepatternlib@[0.11,);")
+public class LUMINA {
+
+}
---
+++ src/main/java/com/falsepattern/lumina/internal/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023 FalsePattern
+ * All Rights Reserved
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This package contains the internal classes of the LUMINA library. Do not use these classes directly.
+ */
+package com.falsepattern.lumina.internal;
