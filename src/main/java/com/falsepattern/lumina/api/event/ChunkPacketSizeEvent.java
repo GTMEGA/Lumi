e26b71417269fd763791137048635622cfad1ba8
FalsePattern <me@falsepattern.com>
2023-11-20 15:41:08 +0100

    update license headers

--- src/main/java/com/falsepattern/lumina/api/event/ChunkPacketSizeEvent.java
+++ src/main/java/com/falsepattern/lumina/api/event/ChunkPacketSizeEvent.java
@@ -1,8 +1,18 @@
 /*
- * Copyright (c) 2023 FalsePattern, Ven
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
- * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ * This file is part of LUMINA.
+ *
+ * LUMINA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LUMINA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LUMINA. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.falsepattern.lumina.api.event;
